7bb5b02b6126df4ad7484598f4dfb2bc12b9bea8
Airidas <skalskimas@gmail.com>
2026-01-12 20:16:37 +0200

    style: remove left-over comment

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -7,5 +7,4 @@ const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL
 });
 
-// This is the single instance used by the whole app
 export const prisma = new PrismaClient({ adapter });
