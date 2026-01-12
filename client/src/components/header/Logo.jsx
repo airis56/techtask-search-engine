4278fd4883036d0d7b79952c228cc892e5a1205f
Airidas <skalskimas@gmail.com>
2026-01-12 20:25:22 +0200

    feat: clickable logo to go back to home page

--- client/src/components/header/Logo.jsx
+++ client/src/components/header/Logo.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Logo() {
     return (
-        <div className="flex items-center shrink-0">
+        <Link to="/" className="flex items-center shrink-0">
             <img
                 src="/logoFull.svg"
                 alt="Eneba"
                 className="w-38.75 h-auto block"
             />
-        </div>
+        </Link>
     );
 }
