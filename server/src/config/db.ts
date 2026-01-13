7a3839d2682f116e97d0e6570ce7bec424ce5e4f
airis56 <skalskimas@gmail.com>
2026-01-13 12:34:37 +0200

    ci: loading dotenv/config only if it's a dev environment

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,6 +1,9 @@
 import { PrismaClient } from "../../prisma/generated/prisma/client";
 import { PrismaPg } from '@prisma/adapter-pg';
-import dotenv from 'dotenv/config';
+import dotenv from 'dotenv';
+
+// only load env vars in dev as railway breaks if there's dotenv.config used in prod
+if (process.env.NODE_ENV !== 'production') dotenv.config();
 
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL
